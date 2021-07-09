b0163ac0bfbd0015658f6560d8660044bcef4c3e
William <willvonarend@gmail.com>
2021-07-09 19:22:14 +0200

    remove unused code in services

--- client/services/ApiService.js
+++ client/services/ApiService.js
@@ -1,53 +1,54 @@
-const bearer_token = require('../dotFile')
+const bearer_token = require('../dotFile');
 
-// const endTime = 1623232800000
-// const startTime = 1623146400000
+const IPAddress = '';
 
-let endTime = Date.now()
-let endDate = new Date(endTime)
-let startTime = endDate.setDate(endDate.getDate()-1)
+let endTime = Date.now();
+let endDate = new Date(endTime);
+let startTime = endDate.setDate(endDate.getDate() - 1);
 
 // ------- Get sleep stage data from Google API
 const getGoogleData = async (token) => {
-  const res = await fetch('https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate', {
+  const res = await fetch(
+    'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: bearer_token.GOOGLE_BEARER_TOKEN,
+      },
+      body: JSON.stringify({
+        aggregateBy: [
+          {
+            dataTypeName: 'com.google.sleep.segment',
+          },
+        ],
+        endTimeMillis: endTime,
+        startTimeMillis: startTime,
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log('error', err);
+    });
+  sendData(res);
+  return res;
+};
+
+// ------- Send sleep stage data from Google API to back-end
+const sendData = async (data) => {
+  const res = await fetch(`http://${IPAddress}:3006/getData`, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
-      Authorization: bearer_token.GOOGLE_BEARER_TOKEN
     },
-    body: JSON.stringify({
-      aggregateBy: [
-        {
-          dataTypeName: 'com.google.sleep.segment'
-        }
-      ],
-      endTimeMillis: endTime,
-      startTimeMillis: startTime
-    })
-  }).then(res => res.json())
-    .catch(err => {
-      console.log('error', err)
-    })
-  sendData(res)
-  //console.log(res);
-  return res
-}
-
-// ------- Send sleep stage data from Google API to back-end   
-// expert: 'http://192.168.1.116:3006/getData' / cazador: 'http://192.168.68.100:3006/getData'
-const sendData = async (data) => {
-  const res = await fetch('http://192.168.1.116:3006/getData', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).catch(err => {
-    console.log('error', err)
-  })
-  return res.json()
-}
+    body: JSON.stringify(data),
+  }).catch((err) => {
+    console.log('error', err);
+  });
+  return res.json();
+};
 
 export default {
-  getGoogleData
-}
+  getGoogleData,
+};
